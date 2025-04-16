5d95f9aabbb627a8d99c186b60ee08100e6a214b
William Ritchie <williamritchie@hotmail.ca>
2025-04-16 13:44:48 -0700

    4k bug fixes!

--- BG3-Text-Chat/Mods/BG3-Text-Chat/ScriptExtender/Lua/Client/Text-Chat_Window.lua
+++ BG3-Text-Chat/Mods/BG3-Text-Chat/ScriptExtender/Lua/Client/Text-Chat_Window.lua
@@ -1,16 +1,16 @@
 local ACTIVE_ALPHA = 0.9
 local INACTIVE_ALPHA = 0.5
-local INPUT_HEIGHT = 35 -- px
 local DEFAULT_SETTINGS_TEXT = "Click and drag me with middle\nmouse button to move me around!\nTo resize me, click and\ndrag on any of my edges!\nClick the \"Save\" when done!"
 local DEFAULT_GREETING = "~ Welcome to the chat ~"
 
 local chat_size = {493, 225}
 local chat_position = {1415, 375}
 local cached_game_window_width = 1920
+local old_cached_game_window_width = cached_game_window_width
 local settings_visible = false
 
 -- Vars for window repositioning and resizing
-local EDGE_SIZE = 2 -- px
+local EDGE_SIZE = 3 -- px
 
 local is_moving = false
 local is_left_resizing = false
@@ -46,7 +46,7 @@ text:SetStyle("Alpha", 1)
 local input_parent = Ext.IMGUI.NewWindow("Input Holder")
 input_parent.NoTitleBar = true
 input_parent:SetPos({chat_position[1], chat_position[2] + chat_size[2]})
-input_parent:SetSize({chat_size[1], INPUT_HEIGHT})
+input_parent:SetSize({chat_size[1], cached_game_window_width < 2250 and 35 or 70})
 input_parent:SetStyle("Alpha", INACTIVE_ALPHA)
 input_parent.NoMove = true
 input_parent.NoResize = true
@@ -56,7 +56,7 @@ input_parent.NoScrollbar = true
 local input = input_parent:AddInputText("")
 input.AllowTabInput = true
 input.EscapeClearsAll = true
-input.ItemWidth = chat_size[1] - 39
+input.ItemWidth = chat_size[1] - cached_game_window_width < 2250 and 69 or 150
 input.OnDeactivate = function()
     TC_SendMessage(input.Text)
     input.Text = ""
@@ -77,7 +77,7 @@ end
 
 local settings_parent = Ext.IMGUI.NewWindow("Settings")
 settings_parent:SetPos(chat_position)
-settings_parent:SetSize({chat_size[1], chat_size[2] + INPUT_HEIGHT})
+settings_parent:SetSize({chat_size[1], chat_size[2] + (cached_game_window_width < 2250 and 35 or 70)})
 settings_parent.Visible = false
 settings_parent.NoTitleBar = true
 settings_parent.NoCollapse = true
@@ -90,8 +90,8 @@ local log = settings_parent:AddText(DEFAULT_SETTINGS_TEXT)
 local settings_button_toggled_holder = Ext.IMGUI.NewWindow("Settings Button Holder")
 settings_button_toggled_holder.SameLine = false
 settings_button_toggled_holder:SetPos({870, 650})
-settings_button_toggled_holder.ItemWidth = 242
-settings_button_toggled_holder:SetSize({190, INPUT_HEIGHT * 8 - 7})
+settings_button_toggled_holder.ItemWidth = 442
+settings_button_toggled_holder:SetSize({290, (cached_game_window_width < 2250 and 35 or 70) * 8 - 7})
 settings_button_toggled_holder.NoTitleBar = true
 settings_button_toggled_holder.Visible = false
 settings_button_toggled_holder.NoMove = true
@@ -138,8 +138,45 @@ game_display_size_3840_button.OnChange = function() if not game_display_size_384
 local clear_button = settings_button_toggled_holder:AddButton("Clear Chat")
 clear_button.SameLine = false
 clear_button.ItemWidth = 178
-clear_button.Size = {178, 25.3}
+clear_button.Size = {178, cached_game_window_width < 2250 and 25.5 or 40}
 clear_button.OnClick = function() text.Label = DEFAULT_GREETING end
+
+local settings_button_toggled = settings_button_toggled_holder:AddButton("Save")
+local settings_button_untoggled = input_parent:AddButton("Settings")
+
+local function _update_windows()
+    TC_DebugPrint("Updating window dimensions")
+    if cached_game_window_width ~= old_cached_game_window_width then
+        -- Reset the window to default, to enter recovery mode
+        chat_position = {1415, 375}
+        chat_size = {493, 225}
+    end
+    old_cached_game_window_width = cached_game_window_width
+    text_parent.NoMove = false
+    text_parent.NoResize = false
+    text_parent:SetPos(chat_position)
+    text_parent:SetSize(chat_size)
+    text_parent.NoMove = true
+    text_parent.NoResize = true
+    input_parent.NoMove = false
+    input_parent.NoResize = false
+    input_parent:SetPos({chat_position[1], chat_position[2] + chat_size[2]})
+    input_parent:SetSize({chat_size[1], (cached_game_window_width < 2250 and 35 or 70)})
+    input_parent.NoMove = true
+    input_parent.NoResize = true
+    input.ItemWidth = chat_size[1] - (cached_game_window_width < 2250 and 69 or 150)
+    settings_button_toggled_holder:SetSize({290, (cached_game_window_width < 2250 and 35 or 50) * 8 - 7})
+    settings_parent:SetPos(chat_position)
+    settings_parent:SetSize({chat_size[1], chat_size[2] + (cached_game_window_width < 2250 and 35 or 70)})
+    settings_button_untoggled.PositionOffset = {chat_size[1] - (cached_game_window_width < 2250 and 66 or 140), cached_game_window_width < 2250 and -27 or -54}
+    clear_button.Size = {178, cached_game_window_width < 2250 and 25.5 or 40}
+    settings_button_toggled.Size = {178, cached_game_window_width < 2250 and 25.5 or 40}
+    game_display_size_1920_button.Active = cached_game_window_width == 1920
+    game_display_size_2560_button.Active = cached_game_window_width == 2560
+    game_display_size_3440_button.Active = cached_game_window_width == 3440
+    game_display_size_3840_button.Active = cached_game_window_width == 3840
+    log.Label = DEFAULT_SETTINGS_TEXT
+end
 
 local function _toggle_settings()
     if settings_visible then
@@ -160,6 +197,8 @@ local function _toggle_settings()
             GameWindowWidth = cached_game_window_width
         }
         TC_SaveWindowSettings(window_save_table)
+
+        _update_windows()
     else
         text_parent.Visible = false
         input_parent.Visible = false
@@ -171,11 +210,10 @@ local function _toggle_settings()
     end
 end
 
-local settings_button_toggled = settings_button_toggled_holder:AddButton("Save")
 settings_button_toggled.SameLine = false
 settings_button_toggled.PositionOffset = {0, 0}
 settings_button_toggled.ItemWidth = 178
-settings_button_toggled.Size = {178, 25.3}
+settings_button_toggled.Size = {178, cached_game_window_width < 2250 and 25.5 or 40}
 settings_button_toggled.OnClick = _toggle_settings
 
 --TC_ShouldDisplayOverheadText = true
@@ -183,42 +221,22 @@ settings_button_toggled.OnClick = _toggle_settings
 --display_overhead_text_toggle.Checked = TC_ShouldDisplayOverheadText
 --display_overhead_text_toggle.OnChange = function() TC_ShouldDisplayOverheadText = not TC_ShouldDisplayOverheadText _P(TC_ShouldDisplayOverheadText and 1 or 0) end
 
-local settings_button_untoggled = input_parent:AddImageButton("Settings Button Untoggled", "9735d896-1c99-412f-88ce-1bc3c129db18", {21, 21})
 settings_button_untoggled.SameLine = false
-settings_button_untoggled.PositionOffset = {chat_size[1] - 36, -27}
-settings_button_untoggled.ItemWidth = 36
+settings_button_untoggled.PositionOffset = {chat_size[1] - (cached_game_window_width < 2250 and 66 or 140), cached_game_window_width < 2250 and -27 or -54}
+settings_button_untoggled.ItemWidth = cached_game_window_width < 2250 and 36 or 72
 settings_button_untoggled.OnClick = _toggle_settings
 
-local function _update_windows()
-    TC_DebugPrint("Updating window dimensions")
-    text_parent.NoMove = false
-    text_parent.NoResize = false
-    text_parent:SetPos(chat_position)
-    text_parent:SetSize(chat_size)
-    text_parent.NoMove = true
-    text_parent.NoResize = true
-    input_parent.NoMove = false
-    input_parent.NoResize = false
-    input_parent:SetPos({chat_position[1], chat_position[2] + chat_size[2]})
-    input_parent:SetSize({chat_size[1], INPUT_HEIGHT})
-    input_parent.NoMove = true
-    input_parent.NoResize = true
-    input.ItemWidth = chat_size[1] - 39
-    settings_parent:SetPos(chat_position)
-    settings_parent:SetSize({chat_size[1], chat_size[2] + INPUT_HEIGHT})
-    settings_button_untoggled.PositionOffset = {chat_size[1] - 36, -27}
-    game_display_size_1920_button.Active = cached_game_window_width == 1920
-    game_display_size_2560_button.Active = cached_game_window_width == 2560
-    game_display_size_3440_button.Active = cached_game_window_width == 3440
-    game_display_size_3840_button.Active = cached_game_window_width == 3840
-    log.Label = DEFAULT_SETTINGS_TEXT
-end
+-- local settings_button_untoggled = input_parent:AddImageButton("Settings Button Untoggled", "9735d896-1c99-412f-88ce-1bc3c129db18", {21, 21})
+-- settings_button_untoggled.SameLine = false
+-- settings_button_untoggled.PositionOffset = {chat_size[1] - 72, -27}--{chat_size[1] - 36, -27}
+-- settings_button_untoggled.ItemWidth = 72--36
+-- settings_button_untoggled.OnClick = _toggle_settings
 
 local function _handle_window_dragging(event)
     if event.Pressed then
         -- Left edge resize
         if event.X >= chat_position[1] - EDGE_SIZE and event.X <= chat_position[1] + EDGE_SIZE
-        and event.Y >= chat_position[2] - EDGE_SIZE and event.Y <= chat_position[2] + chat_size[2] + INPUT_HEIGHT + EDGE_SIZE
+        and event.Y >= chat_position[2] - EDGE_SIZE and event.Y <= chat_position[2] + chat_size[2] + (cached_game_window_width < 2250 and 35 or 70) + EDGE_SIZE
         then is_left_resizing = true log.Label = DEFAULT_SETTINGS_TEXT .. "\n\nResizing left edge..."
         -- Top edge resize
         elseif event.X >= chat_position[1] - EDGE_SIZE and event.X <= chat_position[1] + chat_size[1] + EDGE_SIZE
@@ -226,15 +244,15 @@ local function _handle_window_dragging(event)
         then is_top_resizing = true log.Label = DEFAULT_SETTINGS_TEXT .. "\n\nResizing top edge..."
         -- Right edge resize
         elseif event.X >= chat_position[1] + chat_size[1] - EDGE_SIZE and event.X <= chat_position[1] + chat_size[1] + EDGE_SIZE
-        and event.Y >= chat_position[2] - EDGE_SIZE and event.Y <= chat_position[2] + chat_size[2] + INPUT_HEIGHT + EDGE_SIZE
+        and event.Y >= chat_position[2] - EDGE_SIZE and event.Y <= chat_position[2] + chat_size[2] + (cached_game_window_width < 2250 and 35 or 70) + EDGE_SIZE
         then is_right_resizing = true log.Label = DEFAULT_SETTINGS_TEXT .. "\n\nResizing right edge..."
         -- Bottom edge resize
         elseif event.X >= chat_position[1] - EDGE_SIZE and event.X <= chat_position[1] + chat_size[1] + EDGE_SIZE
-        and event.Y >= chat_position[2] + chat_size[2] + INPUT_HEIGHT - EDGE_SIZE and event.Y <= chat_position[2] + INPUT_HEIGHT + chat_size[2] + EDGE_SIZE
+        and event.Y >= chat_position[2] + chat_size[2] + (cached_game_window_width < 2250 and 35 or 70) - EDGE_SIZE and event.Y <= chat_position[2] + (cached_game_window_width < 2250 and 35 or 70) + chat_size[2] + EDGE_SIZE
         then is_bottom_resizing = true log.Label = DEFAULT_SETTINGS_TEXT .. "\n\nResizing bottom edge..."
         -- Move
         elseif event.X > chat_position[1] + EDGE_SIZE and event.X < chat_position[1] + chat_size[1] - EDGE_SIZE
-        and event.Y > chat_position[2] + EDGE_SIZE and event.Y < chat_position[2] + chat_size[2] + INPUT_HEIGHT - EDGE_SIZE
+        and event.Y > chat_position[2] + EDGE_SIZE and event.Y < chat_position[2] + chat_size[2] + (cached_game_window_width < 2250 and 35 or 70) - EDGE_SIZE
         then is_moving = true log.Label = DEFAULT_SETTINGS_TEXT .. "\n\nMoving..."
         end
 
